c821a031e288a72a2e9e587418de9bffd89e22cb
Khlustov <khlustov.alex@gmail.com>
2021-03-06 16:29:46 +0300

    feat: add 2 pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Description from './components/description'
+import Square from './components/Square'
+import Text from './components/ItalicText'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/square">Square</Link>
+            </li>
+            <li>
+              <Link to="/italictext">Italic text</Link>
+            </li>
+            
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+
+          <Route path="/italictext">
+            <Homework2 />
+          </Route>
+
+          <Route path="/square">
+            <Homework1 />
+          </Route>
+         
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div className = 'content'>
+      <h2>Home</h2>
     </div>
   );
 }
 
-export default App;
+function Homework1() {
+  return (
+    <div className = 'content'>
+      <Description
+      text = 'Стили и текст внутри квадратов передаються с помощью props'
+      />
+      
+      <Square 
+        text = 'Big'
+        cssClass = 'big'
+      />
+      <Square 
+        text = 'Medium'
+        cssClass = 'medium'
+      />
+      <Square 
+        text = 'Small'
+        cssClass = 'small'
+      />
+    </div>
+  );
+}
+
+function Homework2() {
+  return (
+    <div className = 'content'>
+       <Description
+      text = 'По клику на текст он становится курсивом, по клику еще раз, становится нормальным'
+      />
+      <Text text = 'REACT' />
+    </div>
+  );
+}
+
+export default App
+
+
---
+++ src/components/description.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+
+class Description extends Component {
+
+    render() {
+      return(
+        <div>
+           {this.props.text}
+        </div>
+      )
+    }
+  }
+
+export default Description
